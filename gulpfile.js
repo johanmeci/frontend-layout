70785806e90cb6f1746f99590be0acd203ccab9e
Johan Meneses <johanmeci03@gmail.com>
2022-06-28 09:30:19 -0500

    Added gulp config, images and icons

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+const gulp = require('gulp')
+const browserSync = require('browser-sync').create()
+const sass = require('gulp-sass')(require('sass'))
+
+gulp.task('serve', function () {
+  
+  browserSync.init({
+    server: {
+      baseDir: './public',
+      index: 'index.html'
+    }
+  })
+
+  //Add minify
+  gulp.watch('public/sass/*.scss', gulp.series('sass'))
+  gulp.watch('public/*.html').on('change', browserSync.reload)
+
+})
+
+gulp.task('sass', function () {
+
+  return gulp.src('public/sass/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('public/css'))
+    .pipe(browserSync.stream())
+})
+
+gulp.task('start', gulp.series('serve', 'sass'))
