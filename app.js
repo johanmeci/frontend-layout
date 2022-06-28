9fb6d0b4df7cc9090f24371d07d37c981419c7ab
Johan Meneses <johanmeci03@gmail.com>
2022-06-28 09:23:08 -0500

    First commit and express config

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+//Static files
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/js', express.static(__dirname + 'public/js'))
+app.use('/img', express.static(__dirname + 'public/img'))
+
+app.get('', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html')
+})
+
+//Listen port
+app.listen(port, () => console.info(`Listening on port ${port}`))
